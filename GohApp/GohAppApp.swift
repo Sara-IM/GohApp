a4c91d1d768d3c90b0238cfbbb04afc0ed44d184
Amal-Alhamad <amalhamad1419@gmail.com>
2023-10-09 15:44:18 +0300

    Initial Commit

---
+++ GohApp/GohAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GohAppApp.swift
+//  GohApp
+//
+//  Created by Amal Saad on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct GohAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
