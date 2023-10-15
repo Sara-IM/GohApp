bb0805135e755e2b4d85ab880c67e24cd406d3e2
maha <vipmha@hotmail.com>
2023-10-15 17:56:09 +0300

    maha update2

--- GohApp/DiscoverView.swift
+++ GohApp/DiscoverView.swift
@@ -1,18 +1,112 @@
 //
-//  DiscoverView.swift
+//  kkkk.swift
 //  GohApp
 //
-//  Created by Sara Ibrahim Almashharawi on 15/10/2023.
+//  Created by maha on 30/03/1445 AH.
 //
 
 import SwiftUI
 
 struct DiscoverView: View {
+    // let literalColor = Color(#colorLiteral(red: 0.3589375615, green: 0.5140314698, blue: 0.455712378, alpha: 1))
+    // @State var index = 0
+    @State var userLocation : String = " "
+    @State var phoneNumber : Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      
+        @State var searchText = ""
+
+       
+            NavigationStack {
+                
+                
+                
+                ScrollView {
+                
+
+              
+                    VStack(spacing: 10) {
+                        Text("\(searchText)")
+
+                            VStack{
+                                
+                                HStack{
+                                  //  ZStack{
+                                        Image("Mu1")
+                                            .resizable()
+                                            .frame(width:150 , height: 150)
+                                            .cornerRadius(20)
+                                         
+                                    
+                                    
+                                    Image("Mu2")
+                                        .resizable()
+                                        .frame(width:150 , height: 150)
+                                        .cornerRadius(20)
+//                                    VStack{
+//                                        Spacer()
+//                                    }
+                                    
+                                  //  }
+                                  
+                                }
+                                
+                               // Spacer()
+                                
+                             
+                                   // Spacer()
+                              
+                                
+                                HStack{
+                                    VStack{
+                                       // Spacer()
+                                   
+                                    
+                                            Image("Mu3")
+                                                .resizable()
+                                                .frame(width:150 , height: 150)
+                                                .cornerRadius(20)
+                                        
+                                             
+                                      //  Spacer()
+                                    }
+                             
+                                        Image("Mu4")
+                                            .resizable()
+                                            .frame(width:150 , height: 150)
+                                            .cornerRadius(20)
+                                          //  .frame(maxWidth: 190)
+                                        
+                                    
+                                        
+                                   // }
+                                }
+                            }
+                    }
+                }
+                .navigationTitle("Discover")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button(action: {
+                            
+                            
+                        }, label: {
+                           Image(systemName: "person")
+                                .padding(.top, 90)
+                            
+                        })
+                    }
+                }
+            .searchable(text: $searchText)
+            }
+           // .padding()
     }
+        
 }
+
 
 #Preview {
     DiscoverView()
 }
+
