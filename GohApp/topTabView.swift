eb7304390d8ec8dfefc7584a64b8adf7f96bb89e
Amal-Alhamad <amalhamad1419@gmail.com>
2023-10-17 00:13:28 +0300

    update of Invitations and create pages

---
+++ GohApp/topTabView.swift
@@ -0,0 +1,18 @@
+//
+//  topTabView.swift
+//  GohApp
+//
+//  Created by Amal Saad on 02/04/1445 AH.
+//
+
+import SwiftUI
+
+struct topTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    topTabView()
+}
