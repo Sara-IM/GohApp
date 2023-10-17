072c274c111e4935d5577370787fa63a8d65c92d
Amal-Alhamad <amalhamad1419@gmail.com>
2023-10-17 14:04:27 +0300

    // new update for invitation and create page and new page with name topTabView

--- GohApp/createPage.swift
+++ GohApp/createPage.swift
@@ -1,75 +1,24 @@
-//
-//  ContentView.swift
-//  GohApp
-//
-//  Created by Amal Saad on 24/03/1445 AH.
-//
 
 import SwiftUI
 
 struct createPage: View {
-   // let literalColor = Color(#colorLiteral(red: 0.3589375615, green: 0.5140314698, blue: 0.455712378, alpha: 1))
-   // @State var index = 0
+
     @State var userLocation : String = " "
     @State var phoneNumber : Int = 0
     @State var index = 0
     @State private var isAccepted = Bool()
     var body: some View {
-        NavigationStack {
+       // NavigationView {
             VStack(alignment: .leading, spacing: 24){
-                
-                //trailing right
-                //.foregroundColor(Color("textFieldG")) to up our colors
-              //  Text("Hiiiiiii")
+      
                 Text("Create your journey").bold().font(.largeTitle)
                     .foregroundColor(Color("titleGray"))/*.opacity(0.3)*/.cornerRadius(10)
                 
                 Divider()
+                    .padding(.bottom,40)
                 //--------------------------------------------------
-                HStack{//Hstack 1
-                    Text("Create")
-                        .foregroundColor(self.index == 0 ? .white: Color("Green").opacity(0.7))
-                        .fontWeight(.bold).font(.title3)
-                        .padding(.horizontal, 40)//55
-                        .padding(.vertical, 9)//12
-                        .background(Color("Green")
-                            .opacity(self.index == 0 ? 1 : 0 ))
-                        .clipShape(Capsule())
-                        .onTapGesture {
-                            self.index = 0
-                            
-                        }
-                    Spacer(minLength: 0)
-                    
-                    Text("Invitations")//d
-                        .foregroundColor(self.index == 1 ? .white: Color("Green").opacity(0.7))//d
-                        .fontWeight(.bold).font(.title3)
-                    // .padding(.horizontal, 5)// vertical//d
-                        .padding(.horizontal, 30)//52
-                        .padding(.vertical, 9)//35
-                        .background(Color("Green")
-                            .opacity(self.index == 1 ? 1 : 0 ))
-                        .clipShape(Capsule())//d
-                        .onTapGesture {
-                            self.index = 1   //1
-                        }//  .background(
-                }//end Hstack1
-//                .background(
-//                    NavigationLink(" ", destination: invitationsPage(), isActive: $isAccepted)
-//                )
-                .frame(width: 306, height: 48)//end stack
-                .background(Color("titleGray").opacity(0.04))//.border(Color("titleGray")))//0.07
-                //the number of opacity is the dgree of bule color is 0.06
-                .clipShape(Capsule())//.border(Color("titleGray"))
-                
-                .padding(.horizontal )//horizontal
-                .padding(.top , 40)
-                .padding(.bottom,40)
-                //  .padding()
-                // Spacer(minLength: 0)
-                
                 //----------------------------------------------------
-                
+           
                 Label("Your Location", systemImage: " ")
                 TextField(" ", text: $userLocation)
                     .padding()
@@ -79,7 +28,7 @@ struct createPage: View {
                 HStack{
                     TextField(" ", text: $userLocation)
                         .padding()
-                        .background(Color("textFieldG"  )).cornerRadius(10)
+                        .background(Color("textFieldG")).cornerRadius(10)
                     Image(systemName:"plus.circle").font(.system(size: 40)).foregroundColor((Color("Green")))
                 }
                 
@@ -98,15 +47,17 @@ struct createPage: View {
                 
                 
             }//end VStack1
-        }// end NavigationView
-        .padding()
+     //  }// end NavigationView
+            .padding()
+            .padding(.bottom, 170)
     }// end View
 }//end ContentView
-#Preview {
-    ContentView()
-}
+//#Preview {
+//    ContentView()
+//}
 
 
 #Preview {
     createPage()
 }
+
--- GohApp/topTabView.swift
+++ GohApp/topTabView.swift
@@ -1,18 +1,101 @@
 //
-//  topTabView.swift
-//  GohApp
-//
-//  Created by Amal Saad on 02/04/1445 AH.
-//
 
 import SwiftUI
 
 struct topTabView: View {
+    @State var selected = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       NavigationStack {
+            GeometryReader { _ in
+                Topbar(selected: self.$selected)
+                
+            }
+            .background(Color("Color")
+                .edgesIgnoringSafeArea(.all))
+           
+
+            if (selected == 0){
+                createView()
+            }
+            if (selected == 1){
+                invitationsView()
+            }
+            
+       }//end nav
     }
+
 }
 
 #Preview {
     topTabView()
 }
+
+
+struct Topbar : View{
+    @Binding var selected : Int
+    var body: some View{
+        NavigationStack {
+            HStack{
+                Button(action: {
+                    self.selected = 0
+                }) {
+                    Text("Create")
+                    //.resizable()
+                        .frame(width: 90 , height: 25)
+                        .padding(.vertical, 12)
+                        .padding(.horizontal, 30)
+                        .background(self.selected == 0 ? Color("Green") : Color.clear)
+                        .clipShape(Capsule())
+                }
+                .foregroundColor(self.selected == 0 ? Color("whaite"): Color("Green"))
+                
+                
+                Button(action: {
+                    self.selected = 1
+                }) {
+                    //Image("acc")
+                    Text("Invitations").font(.title3)
+                    //   .resizable()
+                        .frame(width: 90 , height: 25)
+                        .padding(.vertical, 12)
+                        .padding(.horizontal, 30)
+                        .background(self.selected == 1 ? Color("Green") : Color.clear)
+                        .clipShape(Capsule())
+                }
+                .foregroundColor(self.selected == 1 ? Color("whaite") : Color("Green"))
+                
+                
+                
+            }
+          //  Spacer()
+            .padding(3)
+            .background(Color("textFieldG"))
+           // .opacity(0.4)
+            .clipShape(Capsule())
+            .padding(.leading, 40)
+            .padding(.top, 40)
+    
+        }
+        
+    }
+    
+}
+
+struct createView: View {
+    @State var SegmentedChoice = 0
+    var body: some View {
+        ZStack {
+            createPage()
+            }
+    }
+}
+
+struct invitationsView: View {
+    @State var SegmentedChoice = 0
+    var body: some View {
+        ZStack {
+            invitationsPage()
+            }
+    }
+}
+ 
