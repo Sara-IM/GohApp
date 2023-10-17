44f06c28dbc056b4f731dc15bbc4eef834622ca9
Amal-Alhamad <amalhamad1419@gmail.com>
2023-10-17 20:25:49 +0300

    update the invitation, create and toptabview pages, also create mapPage

---
+++ GohApp/mapPage.swift
@@ -0,0 +1,18 @@
+//
+//  mapPage.swift
+//  GohApp
+//
+//  Created by Amal Saad on 02/04/1445 AH.
+//
+
+import SwiftUI
+
+struct mapPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mapPage()
+}
